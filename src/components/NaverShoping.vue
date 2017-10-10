c230842e0ee8a9ff67fa4bbba169360bcd167f19
yusunglee2074 <lys0333@gmail.com>
2017-10-10 11:55:32 +0900

    네이버 쇼핑 일시정지

---
+++ src/components/NaverShoping.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <dropzone id="myVueDropzone" url="https://httpbin.org/post" v-on:vdropzone-success="showSuccess">
+        <!-- Optional parameters if any! -->
+        <input type="hidden" name="token" value="xxx">
+    </dropzone>
+  </div>
+</template>
+
+<script>
+import Dropzone from 'vue2-dropzone'
+import firebase from 'firebase'
+
+export default {
+  components: {
+    Dropzone
+  },
+  name: 'DocList',
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    let ref = firebase.database().ref('naver')
+    ref.set({
+      NAVER: 'ysuuwefjiowefjiowe f wejiofwej iofj weiofj weiofj iowefj iowefj iowefj weiofj weiofj wioefj weiof jweiof wejiofw ej\n weiofjweiofjiowejfiowejfiowejfiowejfiowejfiowefj weiofj weiofj weiofjweiong'
+    })
+  },
+  methods: {
+  },
+  updated () {
+  }
+}
+</script>
+
+<style>
+</style>
